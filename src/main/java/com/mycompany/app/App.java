eccbf9de691e4d71c5869b9fe2da616a3e29af9b
RafiAwsDevops <rafiawsdevops1@gmail.com>
2025-12-09 13:09:02 +0530

    Create App.java

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+public class App {
+    public static String message() {
+        return "Hello from App!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+}
